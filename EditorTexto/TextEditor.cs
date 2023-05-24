89caf200000f3cacd8acda194cc51fd14bc2bf2d
Manu <emanuellybs14@gmail.com>
2023-05-24 08:27:46 -0300

    class TextEditor

---
+++ EditorTexto/TextEditor.cs
@@ -0,0 +1,29 @@
+public class TextEditor : ITextEditor
+{
+    private string Text;
+    private string SavedText;
+    public void Delete()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RedoAction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SaveChanges()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UndoAction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string Write()
+    {
+        throw new System.NotImplementedException();
+    }
+}
